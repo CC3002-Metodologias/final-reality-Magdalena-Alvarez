4761034dcc4f775ec25f896e3977186b2b436160
Magdalena-Alvarez <70860615+Magdalena-Alvarez@users.noreply.github.com>
2021-01-05 11:25:06 -0300

    Delete InvalidMethodException.java

--- src/main/java/com/github/cc3002/finalreality/controller/phases/InvalidMethodException.java
+++
@@ -1,7 +0,0 @@
-package com.github.cc3002.finalreality.controller.phases;
-
-public class InvalidMethodException extends Exception{
-    public InvalidMethodException(String message) {
-        super(message);
-    }
-}
